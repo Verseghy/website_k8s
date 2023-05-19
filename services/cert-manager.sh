2bf5c0e49b4b2859a8a678c12f6743b2570f1ba8
Zoltan Szepesi <zoltan.szepesi@ibm.com>
2023-05-19 17:01:42 +0200

    Update cert-manager

--- services/cert-manager.sh
+++ services/cert-manager.sh
@@ -1,2 +1,2 @@
-kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.11.1/cert-manager.yaml
+kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.12.0/cert-manager.yaml
 kubectl apply -f cert-manager-cluster-issuer.yaml
